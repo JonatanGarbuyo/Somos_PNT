665673a712a22b312427195ddf7b7ed9c093be02
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2023-08-23 23:56:58 -0300

    Add Main program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,18 @@
 package org.example;
 
+import org.example.products.Beverage;
+import org.example.products.Fruit;
+import org.example.products.HairProduct;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cart cart = new Cart();
 
-        
+        cart.addProduct(new Beverage("Coca-Cola Zero", 20, "1.5"));
+        cart.addProduct(new Beverage("Coca-Cola", 18, "1.5"));
+        cart.addProduct(new HairProduct("Shampoo Sedal", 19, "500ml"));
+        cart.addProduct(new Fruit("Frutillas", 64, "kilo"));
+
+        cart.showProducts();
     }
 }
